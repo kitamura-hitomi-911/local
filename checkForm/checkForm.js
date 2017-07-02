85a4d61c8a1e92358d47849e08ce3fd0568bbffc
hitomi <hinitial@gmail.com>
2017-07-02 19:48:16 +0900

    チェックフォーム 基本コード

--- checkForm/checkForm.js
+++ checkForm/checkForm.js
@@ -3,77 +3,144 @@
  */
 
 window.jQuery && (function($, _){
-	"use strict";
-	/**
-	 * セレクトメニューとラベル要素の連動、jqueryプラグインとして使用想定
-	 * @param obj
-	 *   obj.tgt_elm       : jQueryオブジェクト（必須、jQueryプラグインから必ず渡される）
-	 *   obj.select_exp    : tgt_elm 内の select要素 のセレクタ文字列（任意）
-	 *   obj.select_p_exp  : tgt_elm 内の セレクトされている内容の表示用要素のセレクタ文字列（任意）
-	 *   obj.changeCallback: セレクト変更時のコールバック関数（任意、デフォルトはnull）
-	 *                       parent を指定したい場合は {fn:コールバック関数,parent:親} という形で渡す
-	 *   obj.is_exec_change_callback_at_init @type {boolean}
-	 *                     : changeCallback が指定されている場合、初期化時にも処理するか否か（任意、デフォルトはfalse）
-	 * @constructor
-	 */
-	var SetLabelOfSelectBox = function(obj){
-		if(!obj || !obj.tgt_elm) return;
-		this.tgt_elm = obj.tgt_elm;
-		// セレクト
-		this.tgt_select = this.tgt_elm.find(obj.select_exp || 'select');
-		// セレクトされている内容の表示用
-		this.tgt_select_p = obj.select_p_exp?this.tgt_elm.find(obj.select_p_exp):this.tgt_select.prev('p');
-		// セレクト変更後のコールバック
-		this.changeCallback = obj.changeCallback || null;
-		// セレクト変更後のコールバックをinit時にも処理するか
-		this.is_exec_change_callback_at_init = obj.is_exec_change_callback_at_init || false;
-		// セレクトされている要素
-		this.current_select = null;
-		if(this.tgt_select.length){
-			this.init();
-		}
-	};
-	SetLabelOfSelectBox.prototype.init = function(){
-		var that = this;
-		// 初期処理
-		this.current_select = this.tgt_select.find('option:selected');
-		this.setLabel();
-		if(this.is_exec_change_callback_at_init){
-			this.onChangeEvent();
-		}
-		// プルダウン変更時処理
-		this.tgt_select.change(function(){
-			that.current_select = $(this).find('option:selected');
-			that.setLabel();
-			that.onChangeEvent();
-			return;
-		});
-	};
-	SetLabelOfSelectBox.prototype.setLabel=function(){
-		this.tgt_select_p.text(this.current_select.text());
-		return;
-	};
-	SetLabelOfSelectBox.prototype.onChangeEvent=function(){
-		var that = this;
-		if(this.changeCallback){
-			if(typeof(this.changeCallback) == "function"){
-				this.changeCallback(this.current_select);
-			}else if(typeof(this.changeCallback.fn) == "function"){
-				var parent = this.changeCallback.parent || this;
-				this.changeCallback.fn.call(parent,this.current_select);
+	var checkForm = {
+		$tgt_form:null,
+		err_msg:{
+			text_empty:'入力してください',
+			checked_empty:'選択してください',
+			selected_empty:'選択してください'
+		},
+		init:function(op){
+			var op = op || {};
+			this.$tgt_form = $('#'+op.tgt_form_id);
+			if(!this.$tgt_form.length){
+				console.log('処理対象のformがありません');
 			}
-		}
-	};
-	$.fn.setLabelOfSelectBox = function(op) {
-		if(!op){
-			op = {};
-		}
-		return this.each(function(){
-			var $this = $(this);
-			if(!$this.length){return;}
-			op.tgt_elm = $this;
-			new SetLabelOfSelectBox(op);
-		});
-	};
+			return;
+		},
+		check:function(){
+			var that = this;
+			// 初期化
+			var err_flg = false;
+			this.$tgt_form.find('p.error').remove();
+			this.$tgt_form.find('dl.error').removeClass('error');
+			// テキストエリア
+			var err_flg = (function(){
+					var err_flg = false;
+					var err_msg = [];
+					var $tgt_elm = that.$tgt_form.find('dl.edit.text');
+					var $tgt_input = $tgt_elm.length ? $tgt_elm.find('input') : null;
+					if($tgt_input.length){
+						var val = $tgt_input.val();
+						// 未入力チェック
+						if($tgt_elm.hasClass('required') && !val){
+							err_flg = true;
+							err_msg.push(that.err_msg.text_empty);
+						}
+						if(err_flg){
+							$tgt_elm.addClass('error').find('dd').append('<p class="error">'+err_msg.join('<br>')+'</p>');
+						}
+					}
+					return err_flg;
+			})()||err_flg;
 
+			// ラジオボタン
+			var err_flg = (function(){
+					var err_flg = false;
+					var err_msg = [];
+					var $tgt_elm = that.$tgt_form.find('dl.edit.radio');
+					var $tgt_input = $tgt_elm.length ? $tgt_elm.find('input') : null;
+					if($tgt_input.length){
+						var checked = $tgt_elm.find('input:checked');
+						// 未入力チェック
+						if($tgt_elm.hasClass('required') && !checked.length){
+							err_flg = true;
+							err_msg.push(that.err_msg.checked_empty);
+						}
+						if(err_flg){
+							$tgt_elm.addClass('error').find('dd').append('<p class="error">'+err_msg.join('<br>')+'</p>');
+						}
+					}
+					return err_flg;
+				})()||err_flg;
+
+			// チェックボックス
+			var err_flg = (function(){
+					var err_flg = false;
+					var err_msg = [];
+					var $tgt_elm = that.$tgt_form.find('dl.edit.checkbox');
+					var $tgt_input = $tgt_elm.length ? $tgt_elm.find('input') : null;
+					if($tgt_input.length){
+						var checked = $tgt_elm.find('input:checked');
+						// 未入力チェック
+						if($tgt_elm.hasClass('required') && !checked.length){
+							err_flg = true;
+							err_msg.push(that.err_msg.checked_empty);
+						}
+						if(err_flg){
+							$tgt_elm.addClass('error').find('dd').append('<p class="error">'+err_msg.join('<br>')+'</p>');
+						}
+					}
+					return err_flg;
+				})()||err_flg;
+
+			// セレクトボックス
+			var err_flg = (function(){
+					var err_flg = false;
+					var err_msg = [];
+					var $tgt_elm = that.$tgt_form.find('dl.edit.selectbox');
+					var $tgt_input = $tgt_elm.length ? $tgt_elm.find('select') : null;
+					if($tgt_input.length){
+						var selected = $tgt_input.find('option:selected');
+						// 未選択チェック
+						if($tgt_elm.hasClass('required') && !selected.val()){
+							err_flg = true;
+							err_msg.push(that.err_msg.selected_empty);
+						}
+						if(err_flg){
+							$tgt_elm.addClass('error').find('dd').append('<p class="error">'+err_msg.join('<br>')+'</p>');
+						}
+					}
+					return err_flg;
+				})()||err_flg;
+
+			// テキストエリア
+			var err_flg = (function(){
+					var err_flg = false;
+					var err_msg = [];
+					var $tgt_elm = that.$tgt_form.find('dl.edit.textarea');
+					var $tgt_input = $tgt_elm.length ? $tgt_elm.find('textarea') : null;
+					if($tgt_input.length){
+						var val = $tgt_input.val();
+						// 未選択チェック
+						if($tgt_elm.hasClass('required') && !val){
+							err_flg = true;
+							err_msg.push(that.err_msg.text_empty);
+						}
+						if(err_flg){
+							$tgt_elm.addClass('error').find('dd').append('<p class="error">'+err_msg.join('<br>')+'</p>');
+						}
+					}
+					return err_flg;
+				})()||err_flg;
+			return !err_flg;
+		},
+		submit:function(){
+			if(this.check()){
+				this.$tgt_form.submit();
+			}else{
+				// エラーメッセージのある位置までスクロール
+				var $first_error_elm = this.$tgt_form.find('dl.error:eq(0)');
+				if($first_error_elm.length){
+					var first_error_elm_offset_top = $first_error_elm.offset().top;
+					var current_scroll_top = document.documentElement.scrollTop || document.body.scrollTop;
+					if(first_error_elm_offset_top < current_scroll_top){
+						$('html,body').animate({scrollTop: first_error_elm_offset_top}, 500, 'swing');
+					}
+				}
+			}
+			return;
+		}
+	};
+	window.checkForm = checkForm;
 }).call(this, jQuery, _);
