bd57e8c2e3521f09af15cc7f1dcea8f0a4f5adf5
hitomi <hinitial@gmail.com>
2018-05-01 22:40:15 +0900

    調整中

--- vue_modal/common.js
+++ vue_modal/common.js
@@ -63,6 +63,8 @@
 	}();
 
 	console.log(supportsPassive);
+
+
 	/**
 	 * addEventListener のoptionパラメータオブジェクト対応・未対応を吸収
 	 * @param target
@@ -81,12 +83,13 @@
 		target.addEventListener(type, handler, optionsOrCapture);
 	}
 
-	/*
+	/**
 	 * スムーススクロール
-	 * @param {HTML element}
-	 * ある一定の距離以下の場合は距離に対してスピードを変更
+	 * @param {HTML element | string} tgt
+	 * @param {string} [easing]
+	 * @returns {boolean}
 	 */
-	window.smoothScroll = function(tgt) {
+	window.smoothScroll = function(tgt,easing) {
 		var tgt_elm;
 		if(!tgt){
 			return false;
@@ -97,8 +100,7 @@
 			}else{
 				tgt_elm = document.querySelector(tgt);
 			}
-		}
-		if(tgt.nodeType && obj.nodeType ===1){
+		}else if(tgt.nodeType && obj.nodeType ===1){
 			tgt_elm = tgt;
 		}
 		if(!tgt_elm){
@@ -106,23 +108,14 @@
 		}
 
 		var _scroll_tgt;
-		var _current_pos = document.documentElement.scrollTop || document.body.scrollTop;
-		var _tgt_pos = tgt_elm.getBoundingClientRect().top + _current_pos;
-		var _distance = _tgt_pos - _current_pos;
-		var _speed = Math.abs(_distance) > 1333 ? 1000 : 750;
-		var _distance_per_1ms = _distance / _speed;
+		var _start_pos = document.documentElement.scrollTop || document.body.scrollTop;
+		var _end_pos = tgt_elm.getBoundingClientRect().top + _start_pos;
+		var _distance = _end_pos - _start_pos;
+		var _speed = Math.abs(_distance) > 1333 ? 2000 : 1500;
+		var _easing = easing || 'easeOutQuad';
 		var _ua = window.navigator.userAgent.toLowerCase();
 
-		console.log(tgt_elm,_current_pos,_tgt_pos,_distance_per_1ms);
-		var now = window.performance && (
-			performance.now ||
-			performance.mozNow ||
-			performance.msNow ||
-			performance.oNow ||
-			performance.webkitNow );
-		var getTime = function() {
-			return ( now && now.call( performance ) ) || ( new Date().getTime() );
-		};
+		console.log(tgt_elm,_start_pos,_end_pos);
 
 		if ('scrollingElement' in document) {
 			_scroll_tgt = document.scrollingElement;
@@ -131,24 +124,48 @@
 		}else{
 			_scroll_tgt = document.getElementsByTagName('body')[0];
 		}
-		var start_time = getTime();
+
+		var _start_time = getTime();
 		( function loop(){
-			var last_time = getTime();
-			var elapsed_time = Math.floor(last_time - start_time);
-			_current_pos += _distance_per_1ms * Math.floor(last_time - start_time);
-			if(_current_pos >= _tgt_pos && _distance >= 0 || _current_pos <= _tgt_pos && _distance < 0 || elapsed_time > 10000){
-				_current_pos = _tgt_pos;
-				document.scrollingElement.scrollTop = _current_pos;
+			var _last_time = getTime();
+			var _elapsed_time = Math.floor(_last_time - _start_time);
+			var _percentage = ( _elapsed_time / parseInt(_speed, 10) );
+			_percentage = ( _percentage > 1 ) ? 1 : _percentage;
+			var _current_position = _start_pos + ( _distance * easingPattern(_easing, _percentage) );
+
+			if( _percentage == 1  || _elapsed_time > 10000){
+				document.scrollingElement.scrollTop = _current_position;
 			}else{
-				document.scrollingElement.scrollTop = _current_pos;
+				document.scrollingElement.scrollTop = _current_position;
 				requestAnimationFrame( loop );
 			}
-
-			console.log('現在の位置',_current_pos);
-
-			console.log('経過時間',Math.floor(last_time - start_time)); // ms？
 		} )();
 
+		function getTime(){
+			var now = window.performance && (
+				performance.now ||
+				performance.mozNow ||
+				performance.msNow ||
+				performance.oNow ||
+				performance.webkitNow );
+			return ( now && now.call( performance ) ) || ( new Date().getTime() );
+		}
+		function easingPattern ( type, time ) {
+			var pattern;
+			if ( type === 'easeInQuad' ) pattern = time * time; // accelerating from zero velocity
+			if ( type === 'easeOutQuad' ) pattern = time * (2 - time); // decelerating to zero velocity
+			if ( type === 'easeInOutQuad' ) pattern = time < 0.5 ? 2 * time * time : -1 + (4 - 2 * time) * time; // acceleration until halfway, then deceleration
+			if ( type === 'easeInCubic' ) pattern = time * time * time; // accelerating from zero velocity
+			if ( type === 'easeOutCubic' ) pattern = (--time) * time * time + 1; // decelerating to zero velocity
+			if ( type === 'easeInOutCubic' ) pattern = time < 0.5 ? 4 * time * time * time : (time - 1) * (2 * time - 2) * (2 * time - 2) + 1; // acceleration until halfway, then deceleration
+			if ( type === 'easeInQuart' ) pattern = time * time * time * time; // accelerating from zero velocity
+			if ( type === 'easeOutQuart' ) pattern = 1 - (--time) * time * time * time; // decelerating to zero velocity
+			if ( type === 'easeInOutQuart' ) pattern = time < 0.5 ? 8 * time * time * time * time : 1 - 8 * (--time) * time * time * time; // acceleration until halfway, then deceleration
+			if ( type === 'easeInQuint' ) pattern = time * time * time * time * time; // accelerating from zero velocity
+			if ( type === 'easeOutQuint' ) pattern = 1 + (--time) * time * time * time * time; // decelerating to zero velocity
+			if ( type === 'easeInOutQuint' ) pattern = time < 0.5 ? 16 * time * time * time * time * time : 1 + 16 * (--time) * time * time * time * time; // acceleration until halfway, then deceleration
+			return pattern || time; // no easing, no acceleration
+		};
 
 		return false;
 	};
