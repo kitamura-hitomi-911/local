d965a73e9f76654002dc0fe1ce3628e1aff9e70c
hitomi <hinitial@gmail.com>
2018-12-31 21:13:44 +0900

    エクセルデータ コンバート ツール

--- excel_convert/settings.js
+++ excel_convert/settings.js
@@ -5,7 +5,7 @@ var settings = [
 		input:{
 			charset:"Shift_JIS",//  Shift_JIS or UTF-8 、未指定なら TF-8
 			linedelim:'999999', // 行の終わり。未指定なら "999999"
-			enable_line_from:'3', // 2行め以降、有効な行の始まり。未指定なら3.
+			enable_line_from:3, // 2行め以降、有効な行の始まり。未指定なら3.
 			keys:{
 				"ID":'id',
 				"タイトル":"title",
